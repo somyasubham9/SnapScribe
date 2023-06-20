435ee42b0b73262d29932bbebdf1d6e5c4c87037
Somya Subham Dash <workforssd@gmail.com>
2023-06-20 17:58:03 +0530

    styling changes

--- server/routes/postCapRoute.js
+++ server/routes/postCapRoute.js
@@ -6,8 +6,6 @@ import Post from '../mongodb/models/post.js'
 dotenv.config();
 
 const router = express.Router();
-
-
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
